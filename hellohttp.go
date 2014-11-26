5f675291159fb165979784eac3667bcedbdcb259
Raphael J. Lopes <raphaeljlps@users.noreply.github.com>
2014-11-26 09:24:43 -0200

    Update hellohttp.go

--- hellohttp.go
+++ hellohttp.go
@@ -9,4 +9,12 @@ import (
 func main() {
   port := flag.Int("port", 8080, "a porta que você irá utilizar para acessar o serviço.")
   
+  flag.Parse()
+  
+  http.HandleFunc("/", helloHandler)
+  log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Olá mundo!")
 }
